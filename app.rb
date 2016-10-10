947ad0763846bc8b33e640f778b97c2cf51e5f75
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-10 15:44:57 +0100

    Added view file and seperated out html

--- app.rb
+++ app.rb
@@ -25,7 +25,5 @@ get '/super' do
 end
 
 get '/homer' do
-  "<div style='border: 3px dashed blue'>
-  <img src='http://cdn.mos.cms.futurecdn.net/04c6ea3a88378b441f8a9336638859bd-970-80.jpg'>
-  </div>"
+erb(:index)
 end
